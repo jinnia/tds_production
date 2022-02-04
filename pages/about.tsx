ac2163ffd9ff1cdf55e6600bb295a07780b39cca
jinnia <jinniaravindran@gmail.com>
2022-02-04 08:47:12 +0530

    committed about us design

--- pages/about.tsx
+++ pages/about.tsx
@@ -17,21 +17,51 @@ function About() {
             <Footer /> */}
 
 
-            <div className="flex flex-col p-4 h-full">
-                <div className="w-full h-64 bg-gray-700"></div>
-                <div className="w-full h-full bg-red-700">
-                    <h1 className="inline-block align-middle">TOUR DE SOUTH</h1>
-                    <div className="flex  p-4">
-                        <div className="bg-green-400 h-60 w-1/2 m-1"></div>
-                        <div className="bg-green-400 h-60 w-1/2 m-1"></div>
+            <div className="flex flex-col h-full">
+                <div className="w-full h-80">
+                    <img src="/assets/mountain.png" />
+                </div>
+                <div className="w-full h-full">
+                    <h1 className="inline-block text-center font-bold text-xl p-2">SET OUT ON YOUR SEARCH FOR PLACES TO REJUVENATE WITH ADVENTURE</h1>
+                    <p className="p-2 text-lg font-milonga">We are passionate minds seeking raw, wild and real. Living for us is travelling, creating 
+                    experiences and maintaining a strong bond with nature. </p>
+                    <div className="flex">
+                        <div className="h-auto  w-1/2 m-1 mt-4">
+                            <h1 className="text-center text-xl font-bold">CONNECT MORE PEOPLE WITH NATURE</h1>
+                            <p className="p-2 text-lg font-milonga">Tour de South started off as an effort to share what we feel 
+                            of travel and to connect more people with nature. </p>
+                        </div>
+                        <div className="h-60 w-1/2 m-1">
+                            <img src="/assets/connect_with_nature.png" />
+                        </div>
                     </div>
                     <div className="flex  p-4">
-                        <div className="bg-green-400 h-60 w-1/2 m-1"></div>
-                        <div className="bg-green-400 h-60 w-1/2 m-1"></div>
+                        <div className="h-auto  w-1/2 m-1">
+                            <h1 className="text-center text-xl font-bold">ADVENTURE FILLED WITH TREKKING</h1>
+                            <img src="/assets/trekking.png" />
+                        </div>
+                        <div className="h-60 w-1/2 m-1">
+                            <p className="p-2 text-lg font-milonga">Exclusive offbeat locations and adventure
+                            filled trekking experiences, with all experiences bringing you closer to the planet.  </p>                            
+                        </div>
                     </div>
                     <div className="flex  p-4">
-                        <div className="bg-green-400 h-60 w-1/2 m-1"></div>
-                        <div className="bg-green-400 h-60 w-1/2 m-1"></div>
+                        <div className="h-auto  w-1/2 m-1">
+                        <h1 className="text-center text-xl font-bold">WARMTH AND CHILLS</h1>
+                        <p className="p-2 text-lg font-milonga">Our spaces will give you the warmth you need, and the chills you crave! </p>
+                        </div>
+                        <div className="h-60 w-1/2 m-1">
+                            <img src="/assets/warmth_and_chill.png" />
+                        </div>
+                    </div>
+                    <div className="flex  p-4">
+                        <div className="h-auto  w-1/2 m-1">
+                            <h1 className="text-center text-xl font-bold">INDOOR AND OUTDOOR STAY EXPERIENCES</h1>
+                            <img src="/assets/tent.png" />
+                        </div>
+                        <div className="h-60 w-1/2 m-1">
+                            <p className="p-2 text-lg font-milonga">It will revive your soul, freshen your breathe and cheer you up.  </p>                            
+                        </div>
                     </div>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+            milonga: ['\'Milonga\', cursive;', ...defaultTheme.fontFamily.sans],
+            //sans: ['Battlebridge', ...defaultTheme.fontFamily.sans]
+          }
+    },
+    
   },
   plugins: [],
 }
